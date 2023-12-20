20a035d23b70c4cbf57a7cb59cd403b2d65ed80b
William Bellavance Jr <flintman@flintmancomputer.com>
2023-12-20 17:48:51 -0500

    move over to Evervolv
    Change-Id: If0eafa951368079bc5ff03f5d0c3b2b537e04115

--- ev_channel.mk
+++ ev_channel.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product,$(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,8 +11,10 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7) play
-PRODUCT_NAME := lineage_channel
+PRODUCT_NAME := ev_channel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=channel \
     PRIVATE_BUILD_DESC="channel_retail-user 10 QPYS30.85-23-8-2 c00f57 release-keys"
+
+PRODUCT_CODENAME := Modio
